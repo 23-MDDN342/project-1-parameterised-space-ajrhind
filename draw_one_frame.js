346a58250924537c8eb43dc30b7390caf16c72ff
ajrhind <alanah.rhind009@gmail.com>
2023-03-02 17:38:27 +1300

    push

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -5,10 +5,15 @@ var b=100;
 
 
 function draw_one_frame(cur_frac) {
+	
+	
 
-	let ripple=map(cur_frac,0,1,0,width*2);
+	let ripple=map(cur_frac,0,1,0,width*0.5);
+	fill(255,255,255);//big one 
 	ellipse(width/2,height/2,ripple,ripple);
-	fill(100,0,100)
+	fill(100,0,100);//little one
+	ellipse(width/2,height/2,ripple/2,ripple/2);
+	fill(100,0,100);//bigger
 	ellipse(width/2,height/2,ripple/2,ripple/2);
 
 }
