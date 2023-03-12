b53c007dc694835fcac1389d8e890c5220a21e5a
ajrhind <alanah.rhind009@gmail.com>
2023-03-12 17:05:15 +1300

    Yep

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 **Week Two** 
 
 After getting my simple water ripple loop working, I'm thinking about different ways I could do it. I thought about making a for loop, with lots of ellipses with a heavy stroke, and then when there is a water drop, they will bounce up and down, and change colour. I think this could be quite difficult though, as  would need to have a delay at the start for the water drop. I also have no idea how I would make each ellipes stroke bounce up and down to make the illusion of a ripple. If I can't manage to do it, I think I might just take the drop out all together, and make it just a really smooth illusion. 
+
+For this second class I'm trying to work with a for loop before I commit to using the other method. I think in the long run the for loop will be much easier, but figuring it out right now is the issue. 
--- draw_one_frame.js
+++ draw_one_frame.js
@@ -3,8 +3,8 @@ function draw_one_frame(cur_frac, cur_frame) {
 	//setup-------------------------------
 	let halfX = width/2;
 	let halfY = height/2;
-	let ESX = 100;
-	let ESY = 50;
+	let ESX = 400;
+	let ESY = ESX/2;
 	background(134, 177, 247);
 	strokeWeight(60);
 	//-----------------------------------
@@ -16,12 +16,15 @@ function draw_one_frame(cur_frac, cur_frame) {
 	let strip2=map(cur_frac,0,1,11,30);
 	let strip3=map(cur_frac,0,1,31,60);
 
+	let bob=map(cur_frac,0,1,);
 
 	let HL=color(255)//(177, 206, 250);165, 217, 250 , ,#DAE4F5
 	let LL=color(0);//(52, 190, 250);//115, 198, 250, , #92BCFF
+	let mao=color(155,198,250);
 //mid light  #B7D1FA
 
 	let wadda=lerpColor(LL,HL,cur_frac);
+	
 	//work-------------------------------
 
 		
@@ -67,9 +70,28 @@ function draw_one_frame(cur_frac, cur_frame) {
 fill(0,0,0,0);
 strokeWeight(2);
 	// stroke(104, 146, 212);
-	for(let i=0;i<16;i++){
+	for(let i=0;i<4;i++){
 		stroke(wadda);
 		ellipse(halfX,halfY,ESX*i,ESY*i);
 		// scale(i);
 	}
+	for(let i=0;i<8;i++){
+		stroke(200,255,100);//greeny
+		ellipse(halfX,halfY,(ESX/1.2)*i,(ESY/1.2)*i);
+		}
+
+	for(let i=0;i<8;i++){
+		stroke(255,100,200);//pinky
+		ellipse(halfX,halfY,(ESX/1.4)*i,(ESY/1.4)*i);
+		}
+
+	for(let i=0;i<8;i++){
+		stroke(100,200,255);//bluey
+		ellipse(halfX,halfY,(ESX/1.6)*i,(ESY/1.6)*i);
+		}
+
+	// for(let i=0;i<8;i++){
+	// 	stroke(250,155,155);//ornge
+	// 	ellipse(halfX,halfY,(ESX/1.8)*i,(ESY/1.8)*i);
+	// 	}
 }	
